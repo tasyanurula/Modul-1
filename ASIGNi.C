8b7b41a37bb5d4ef4853b09cde31e68f2c267afa
tasyanurula <62134909+tasyanurula@users.noreply.github.com>
2020-03-20 13:04:43 +0700

    Create ASIGNi.C

---
+++ ASIGNi.C
@@ -0,0 +1,21 @@
+/* File : ASIGNi.C */
+/* Assignement dan print */
+#include <stdio.h>
+#include <limits.h>
+Int
+main ()
+{
+/* Kamus */
+int i;
+long int ii;
+/* Program */
+   printf ("hello\n\n");
+   i = 1234;
+  ii = 123456;
+  printf ("Ini nilai i=1234 = : %d \n", i);
+  printf ("Ini nilai ii=123456 : %10d \n\n", ii);
+/* print nilai batas integer */
+  printf ("Min dan Max integer : %d, %d \n", INT_MIN, INT_MAX);
+  printf ("Max long integer : %ld, %ld \n", LONG_MAX);
+return 0;
+}
