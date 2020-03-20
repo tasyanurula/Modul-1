4d0b0f1afc7663b49edffbe7ed6d972552f9c909
tasyanurula <62134909+tasyanurula@users.noreply.github.com>
2020-03-20 13:02:12 +0700

    Create ASIGN.C

---
+++ ASIGN.C
@@ -0,0 +1,15 @@
+/* File : ASIGN.C */
+/* Assignment nilai integer dan print */
+#include <stdio.h>
+
+int main ()
+{
+/* Kamus */
+int i;
+
+/* Program */
+printf ("hello\n");
+i = 5;
+printf ("Ini nilai i : %d \n", i);
+return 0;
+}
